855617d3b4976b2569bcaaa3ed1b2dd782d9f1da
johnse21 <john21aparejado@gmail.com>
2023-10-17 21:34:20 +0800

    useEffect section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
-import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
-import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
-import UseStateCounter from './tutorial/1-useState/setup/5-useState-counter';
+import UseEffectBasics from './tutorial/2-useEffect/setup/1-useEffect-basics';
+import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 
 
 function App() {
   return (
     <div className='container'>
-      <UseStateArray />
+      <UseEffectFetchData />
     </div>
   );
 }
